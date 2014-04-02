5e6065664bbf04aca65040a2ae00cc56c13ee990
JayLegoMine14 <jaylegomanallison@gmail.com>
2014-04-02 15:29:01 +0200

    test

--- teleportEffects/TeleportEffects.java
+++ teleportEffects/TeleportEffects.java
@@ -17,6 +17,8 @@ import org.kitteh.vanish.staticaccess.VanishNoPacket;
 @SuppressWarnings("deprecation")
 public class TeleportEffects extends JavaPlugin implements Listener{
 	
+	//testing
+	
 	Plugin vanish;
 	boolean useVanish = false;
 	
